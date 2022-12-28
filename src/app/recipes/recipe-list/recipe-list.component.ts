a19fa98a72f596ac8c82faf04c186f30b6a75a9f
Aykut <aykutschuermeyer@pm.me>
2022-12-28 16:41:34 +0700

    feat: static recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Garlic Naan",
+      "Bread, Butter, Garlic",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Butter_Garlic_Naan.jpg/800px-Butter_Garlic_Naan.jpg?20150614174921"
+    ),
+  ];
 }
